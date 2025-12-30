dd2f73a669c6fbcd2c5086bd14d81b9fa3fbc46c
arunt2005 <arunt2005@gmail.com>
2025-12-30 19:09:49 +0530

    fixed

---
+++ client/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <h1 className="text-3xl text-center mt-10">Smart Notes App</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
